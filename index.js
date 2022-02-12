5b2ccc7b0a1763a0fab71049cbb8f29ad4baf1d9
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-12 23:26:09 +0530

    Added Right dice functionality using JavaScript

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Random Number for Dice 1
 var randomNumber1 = Math.floor(Math.random() * 6) + 1
-
 document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png")
+
+// Random Number for Dice 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png")
