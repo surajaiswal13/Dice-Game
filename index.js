a58ab7359659872e2e301e15c326ca0fed056f2f
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-12 23:36:54 +0530

    Optimization

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+
 // Random Number for Dice 2
 var randomNumber2 = Math.floor(Math.random() * 6) + 1
 document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png")
+
+if(randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins 🚩"
+}
+else if(randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins"
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw"
+}
